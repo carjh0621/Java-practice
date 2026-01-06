6331f95fa587ab9f67b5245c6baadd2ead85cca4
carjh0621 <carjh0621@github.com>
2026-01-06 12:39:42 +0900

    Add chapter 4 practice codes

---
+++ project/src/ticket/system/ConcertHall.java
@@ -0,0 +1,77 @@
+class ConcertHall {
+    int row_n=10, col_n=10;
+    Seat[][] seats;
+
+    public ConcertHall(){
+        seats = new Seat[row_n][col_n];
+        for (int i = 0; i < row_n; i++) {
+            for (int j = 0; j < col_n; j++) {
+                seats[i][j] = new Seat();    // 여기서 Seat 객체 생성
+            }
+        }
+    }
+
+    public void showSeats(){
+        //x: booked o: available
+        System.out.println("x: booked, o: available");
+        System.out.print("  ");
+        for(int i=0;i<col_n;i++){
+            System.out.print(" "+i+" ");
+        }System.out.println();
+        for(int i=0;i<row_n;i++){
+            System.out.print(i+" ");
+            for(int j=0;j<col_n;j++){
+                boolean cur_book_info = seats[i][j].isBooked();
+                if(cur_book_info)
+                    System.out.print("[x]");
+                else
+                    System.out.print("[o]");
+            }
+            System.out.println();
+        }
+    }
+
+    public void reserveSeat(int r, int c, String name){
+        if(r>=row_n || r<0 || c>=col_n || c<0)
+            System.out.println("out of index, reserve fail");
+        else if (seats[r][c].isBooked())
+            System.out.println("this seat is already booked, reserve fail");
+        else{
+            if(seats[r][c].book(name)==1)
+                System.out.println("reserve success");
+            else
+                System.out.println("reserve fail");
+        }
+    }
+
+    public void cancelSeat(int r, int c, String name){
+        if(r>row_n || r<0 || c>=col_n || c<0)
+            System.out.println("out of index, cancel fail");
+        else if (!seats[r][c].isBooked()) {
+            System.out.println("this seat is not booked, cancel fail");
+        }
+        else{
+            if(seats[r][c].cancel(name)==1)
+                System.out.println("cancel success");
+            else if(seats[r][c].cancel(name)==0)
+                System.out.println("this seat is not booked, cancel fail");
+            else
+                System.out.println("this seat is not booked by you. cancel fail");
+        }
+    }
+
+    public void findSeat(String name){
+        int count=0;
+        for(int i=0;i<row_n;i++){
+            for(int j=0;j<col_n;j++){
+                if(seats[i][j].bookperson(name)) {
+                    count++;
+                    System.out.printf("%d번째 좌석 좌표(row,col): (%d,%d)\n", count, i, j);
+                }
+            }
+        }
+        if(count==0){
+            System.out.println("예매한 좌석 없음");
+        }
+    }
+}
---
+++ project/src/ticket/system/Seat.java
@@ -0,0 +1,42 @@
+class Seat {
+    private boolean isBooked;
+    private String reserverName;
+
+    public Seat(){
+        this.isBooked=false;
+        this.reserverName="";
+    }
+    //특정 좌석에 대해서 예매를 하는것
+    //n x m 좌석 배열 중 한 좌석에 대한 예매
+    //좌석의 index는 ConcertHall에서 관리
+    public int book(String name){
+       this.reserverName=name;
+       this.isBooked=true;
+       return 1; //예매 성공
+    }
+
+    public int cancel(String name){
+        if(!this.reserverName.equals(name)){
+            return -1; //name not same
+        }
+        else if(!this.isBooked){
+            return 0; //not booked
+        }
+        else{
+            this.reserverName=null;
+            this.isBooked=false;
+            return 1;
+        }
+
+    }
+
+    public boolean isBooked(){
+        return isBooked;
+    }
+
+    public boolean bookperson(String name){
+        return this.reserverName.equals(name);
+    }
+
+
+}
