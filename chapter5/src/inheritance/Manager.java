6eed6c42706425ff8c6e18643f397f1ebe53d77a
carjh0621 <carjh0621@github.com>
2026-01-06 21:53:38 +0900

    chapter5 연습 코드 (5.1까지)

--- chapter5/src/inheritance/Manager.java
+++ chapter5/src/inheritance/Manager.java
@@ -1,4 +1,24 @@
 package inheritance;
 
-public class Manager {
+public class Manager extends Employee {
+    private double bonus;
+
+    // 생성자: 부모 생성자 호출이 필수
+    public Manager(String name, double salary, int year, int month, int day) {
+        // 부모 클래스의 생성자를 가장 먼저 호출해야 함
+        super(name, salary, year, month, day);
+        bonus = 0;
+    }
+
+    // 메서드 오버라이딩 (재정의)
+    @Override
+    public double getSalary() {
+        // 부모의 원래 월급 + 보너스
+        double baseSalary = super.getSalary();
+        return baseSalary + bonus;
+    }
+
+    public void setBonus(double b) {
+        bonus = b;
+    }
 }
--- chapter5/src/inheritance/ManagerTest.java
+++ chapter5/src/inheritance/ManagerTest.java
@@ -1,4 +1,22 @@
 package inheritance;
 
 public class ManagerTest {
+    public static void main(String[] args) {
+        // 1. Manager 객체 생성
+        Manager boss = new Manager("Carl Cracker", 80000, 1987, 12, 15);
+        boss.setBonus(5000);
+
+        // 2. 다형성 활용: Employee 배열에 Manager와 Employee를 섞어서 저장
+        Employee[] staff = new Employee[3];
+
+        staff[0] = boss; // Manager 객체 대입 (가능!)
+        staff[1] = new Employee("Harry Hacker", 50000, 1989, 10, 1);
+        staff[2] = new Employee("Tommy Tester", 40000, 1990, 3, 15);
+
+        // 3. 동적 바인딩 테스트
+        for (Employee e : staff) {
+            // e가 Manager면 Manager의 getSalary(), Employee면 Employee의 getSalary()가 호출됨
+            System.out.println("name=" + e.getName() + ", salary=" + e.getSalary());
+        }
+    }
 }
