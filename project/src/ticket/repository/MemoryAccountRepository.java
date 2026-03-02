16c226011c1601700138df3176bb1e072bc8fab6
carjh0621 <72341225+carjh0621@users.noreply.github.com>
2026-03-02 17:35:52 +0900

    update accountRepo using HashMap

--- project/src/ticket/repository/MemoryAccountRepository.java
+++ project/src/ticket/repository/MemoryAccountRepository.java
@@ -3,13 +3,12 @@ package ticket.repository;
 import ticket.domain.Account;
 import ticket.interfaces.Repository;
 
-import java.util.ArrayList;
-import java.util.List;
-import java.util.Optional;
+import java.util.*;
+
 
 public class MemoryAccountRepository implements Repository<Account, Long> {
-
-    private final List<Account> store = new ArrayList<>();
+    // AccountId(PK) - Account
+    private final Map<Long, Account> store = new HashMap<>();
     private static long sequence = 0L;
 
     @Override
@@ -17,43 +16,39 @@ public class MemoryAccountRepository implements Repository<Account, Long> {
         // 신규 가입
         if(account.getAccountId() == null){
             account.setAccountId(++sequence);
-            store.add(account);
+            store.put(account.getAccountId(),account);
         }
         else{ // 수정,업데이트
-            store.removeIf(existing -> existing.getAccountId().equals(account.getAccountId()));
-            store.add(account);
+            store.put(account.getAccountId(), account);
         }
     }
 
     @Override
     public Optional<Account> findById(Long id) {
-        return store.stream()
-                .filter(account -> account.getAccountId().equals(id))
-                .findFirst();
+        return Optional.ofNullable(store.get(id));
     }
 
     @Override
     public List<Account> findAll() {
-        return new ArrayList<>(store); // 원본 훼손 방지를 위해 복사본 반환
+        return new ArrayList<>(store.values()); // 원본 훼손 방지를 위해 복사본 반환
     }
 
     @Override
     public void delete(Long id) {
-        store.removeIf(account -> account.getAccountId().equals(id));
+        store.remove(id);
     }
 
     // --- Account 전용 특화 조회 메서드 ---
 
     // 로그인용: loginId로 계정 찾기
     public Optional<Account> findByLoginId(String loginId) {
-        return store.stream()
-                .filter(account -> account.getLoginId().equals(loginId))
-                .findFirst();
+        return store.values().stream()
+                .filter(account -> account.getLoginId().equals(loginId)).findFirst();
     }
 
     // 중복가입 방지용: 주민번호(ssn)로 계정 찾기
     public Optional<Account> findByOwnerSsn(String ssn) {
-        return store.stream()
+        return store.values().stream()
                 .filter(account -> account.getOwnerSsn().equals(ssn))
                 .findFirst();
     }
